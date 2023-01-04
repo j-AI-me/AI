d455feab7606ff70e03dcdba4eee91fe97ae1aee
Jaime Garcia Gonzalez (Omnitel Comunicaciones SL) <v-jaimega@microsoft.com>
2023-01-04 02:54:55 +0100

    otra vez

--- client/script.js
+++ client/script.js
@@ -88,7 +88,7 @@ const handleSubmit = async (e) => {
 
     //fetch data from the server
     
-    const response = await fetch('https://jim.onrender.com', {
+    const response = await fetch('https://ia-8or1.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
